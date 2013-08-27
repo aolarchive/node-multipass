c118fee9350c9d84f5861467f7b5190f849c43b9
Jeremy Jannotta <jeremy.jannotta@teamaol.com>
2013-08-27 11:52:22 -0400

    Use ELS authentication for app management
    * Integrate passport-els module, implemented ensureAuthenticated for all views and API routes
    * If not authed, currently just returns a 403 Unauthorized
    * Uses the authed user's GUID for userId in the app model
    * Add GET /api/app route that returns all apps for the current user

--- actors/app/routes.js
+++ actors/app/routes.js
@@ -3,28 +3,51 @@ var passport = require('passport')
   , auth = require('../../auth')
   , appAPI = require('../../data/app')
   , HttpHelper = require('../../routes/httphelper')
-  , ApiResponse = require('../../data/apiresponse');
+  , ApiResponse = require('../../data/apiresponse')
+  , ELSStrategy = require('passport-els').Strategy;
 
+
+/*
+ * Define ELS verification in strategy
+ */
+var els = new ELSStrategy( function(req, done) {
+  this.ELS(req, function(err,user) {
+    if(err) return done(err, null);
+    done(null, user);
+  }); 
+});
+
+/*
+ * Ensure authentication
+ */
+var ensureAuthenticated = function(req, res, next) {
+  els.authenticate(req, null, function(err,user) {
+    if(err) return res.send(403, err);
+    if(user) { 
+      req.user = user;         // add user to request object
+      next(); 
+    }
+  });
+};
 
 module.exports = function(app){
 	
 	var viewsPath = app.get('plugins')['app'] && app.get('plugins')['app']._viewsPath;
 	
-  /*
-  app.get(config.paths.api + '/app',
-    //auth.ensureAuthenticated, 
+	app.get(config.paths.api + '/app',
+    ensureAuthenticated, 
     function(req, res, next) {
       var http = new HttpHelper(req, res),
-        appId = (req.body && req.body.appId) || '';
+      	userId = req.user && req.user.name;
       
-      appAPI.getApp(appId, function(data) {
+      appAPI.getAppsByUserId(userId, function(data) {
         http.send(data);
       });
     }
   );
-  */
+  
   app.post(config.paths.api + '/app',
-    //auth.ensureAuthenticated, 
+    ensureAuthenticated, 
     function(req, res, next) {
       var http = new HttpHelper(req, res);
       
@@ -35,7 +58,7 @@ module.exports = function(app){
   );
   
   app.put(config.paths.api + '/app',
-    //auth.ensureAuthenticated, 
+    ensureAuthenticated, 
     function(req, res, next) {
       var http = new HttpHelper(req, res);
       
@@ -46,7 +69,7 @@ module.exports = function(app){
   );
   
   app.delete(config.paths.api + '/app',
-    //auth.ensureAuthenticated, 
+    ensureAuthenticated, 
     function(req, res, next) {
       var http = new HttpHelper(req, res),
         appId = (req.body && req.body.appId) || '';
@@ -58,7 +81,7 @@ module.exports = function(app){
   );
   
   app.post(config.paths.api + '/app/secret',
-    //auth.ensureAuthenticated, 
+    ensureAuthenticated, 
     function(req, res, next) {
       var http = new HttpHelper(req, res),
         appId = (req.body && req.body.appId) || '';
@@ -71,33 +94,40 @@ module.exports = function(app){
   
   app.configure('development', function(){
     
-    app.get('/app', function(req, res){
-
-      var userId = 'multipass-user';
-        
-      if (!req.param('action')) {
-
-        appAPI.getAppsByUserId(userId, function(data) {
-          res.render(viewsPath + 'app', { userId: userId, apps: data.data });
-        });
-        
-      } else if (req.param('action') == 'create') {
-        
-        res.render(viewsPath + 'appform', { app: { userId: userId } });
-        
-      } else if (req.param('action') == 'edit' && req.param('appId')) {
-        
-        appAPI.getApp(req.param('appId'), function(data) {
-          if (!data.isError()) {
-            res.render(viewsPath + 'appform', { app: data.data });
-          }
-        });
-        
-      } else {
-        next();
-      }
-      
-    });
+    app.get('/app', 
+    	ensureAuthenticated,
+    	function(req, res) {
+    		
+	      var userId = req.user && req.user.name; // Auth guid
+	      
+	      if (!userId) {
+	      	res.send(400, 'Missing auth id');
+	      } 
+	       
+	      if (!req.param('action')) {
+	
+	        appAPI.getAppsByUserId(userId, function(data) {
+	          res.render(viewsPath + 'app', { userId: userId, apps: data.data });
+	        });
+	        
+	      } else if (req.param('action') == 'create') {
+	        
+	        res.render(viewsPath + 'appform', { app: { userId: userId } });
+	        
+	      } else if (req.param('action') == 'edit' && req.param('appId')) {
+	        
+	        appAPI.getApp(req.param('appId'), function(data) {
+	          if (!data.isError()) {
+	            res.render(viewsPath + 'appform', { app: data.data });
+	          }
+	        });
+	        
+	      } else {
+	        next();
+	      }
+	      
+	    }
+    );
     
   });
   
